fc2a69cb7a4d6b67526c608f4496a393fbc9b26c
wali-eldin-hassan <creaspo6@gmail.com>
2023-09-01 23:36:04 +0300

    lazilyResfreshDatabase added

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -2,10 +2,12 @@
 
 namespace Tests;
 
+use Illuminate\Foundation\Testing\LazilyRefreshDatabase;
 use Illuminate\Foundation\Testing\TestCase as BaseTestCase;
 
 abstract class TestCase extends BaseTestCase
 {
     use CreatesApplication;
+    use LazilyRefreshDatabase;
     
 }
